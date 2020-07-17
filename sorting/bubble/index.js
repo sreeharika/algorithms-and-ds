34f9540760f986bc896667d1016c328702010ffe
Sreeharika <sreeharikayelle@gmail.com>
2020-07-17 22:08:34 +0530

    added bubble sort

--- sorting/bubble/index.js
+++ sorting/bubble/index.js
@@ -1,3 +1,16 @@
 // start your code here
+function bubble(arr){
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=0;j<arr.length-1;j++){
+            if(arr[j] > arr[j+1]){
+                var temp = arr[j+1];
+                arr[j+1] = arr[j]
+                arr[j] = temp;
+            }
+        }
+    }
+  return arr;
+}
+console.log(bubble([4,6,1,2,5,3]));
 
 // write the complexity here
